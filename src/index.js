e0b44ede4fe245fdc7c7afb9887c2f3ef6d0cf14
Bárbara Nunes Programmer <barbaranunesprogramer@hotmail.com>
2025-07-02 12:32:04 -0300

    documentações

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const myCart = [];
 console.log ("Welcome to the your Shopee Cart!\nBem-Vindo ao seu carrinho da Shopee!\n")
 
 const item1 = await createItem("Funko Pop",90.99,1);// tive que usar o await para esperar ele criar o item e depois mostrar o subtotal
-const item2 = await createItem("Funko Pop Dobby",90.50,3);
+const item2 = await createItem("Funko Pop Dobby",90.50,3); // aqui ta o item e o valor e a quantidade
 
-await cartService.addItem(myCart,item1);
+await cartService.addItem(myCart,item1); //adiciona o item ao carrinho de compras 
 await cartService.addItem(myCart,item2);
 await cartService.removeItem(myCart,item2)
 
-await cartService.removeItem(myCart,item2)
-await cartService.removeItem(myCart,item2)
+await cartService.removeItem(myCart,item2)//remove um item 
+await cartService.removeItem(myCart,item2)//remove outro item 
 //await cartService.deleteItem(myCart,item2.name)
 
 
